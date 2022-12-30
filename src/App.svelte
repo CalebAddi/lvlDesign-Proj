b09a11c386f8bf148616f16289ef27c15a7656cb
CalebAddi <caleb.addi.77@gmail.com>
2022-12-30 17:18:50 -0600

    Initial Build

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  
+</script>
+
+<main>
+  <body>
+    <div class="align-middle">
+      <h1 class="font-semibold text-red-100">Test Font</h1>
+    </div>
+  </body>
+</main>
+
+<style>
+  
+</style>
